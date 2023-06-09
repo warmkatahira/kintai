76ea61a09b37817304c4ef76b5a54e41d935043f
warmkatahira <t.katahira@warm.co.jp>
2023-06-09 11:04:50 +0900

    test_1

---
+++ public/build/assets/checkbox-32f1fbb1.js
@@ -0,0 +1,1 @@
+function l(){const e=document.getElementsByName("chk[]");let c=0,n=0;for(let t=0;t<e.length;t++)n++,e[t].checked&&c++;return[e,c,n]}export{l as g};
---
+++ public/build/assets/punch_finish-1c649d17.js
@@ -0,0 +1,1 @@
+$(".punch_enter").on("click",function(){$("#punch_id").val($(this).val()),$("#punch_enter_form").submit()});
