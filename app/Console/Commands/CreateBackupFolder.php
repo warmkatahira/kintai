5bf3d356170bb460c9e2df2a84515e30073a8a17
warmkatahira <t.katahira@warm.co.jp>
2023-06-21 10:23:14 +0900

    20230621_1

--- app/Console/Commands/CreateBackupFolder.php
+++ app/Console/Commands/CreateBackupFolder.php
@@ -27,24 +27,7 @@ class CreateBackupFolder extends Command
      */
     public function handle()
     {
-        /* $currentMonth = CarbonImmutable::now()->format('Y-m');
-        Storage::disk('sakura-vps')->makeDirectory("KINTAI/{$currentMonth}"); */
-
         $currentMonth = CarbonImmutable::now()->format('Y-m');
-        $disk = Storage::disk('sakura-vps');
-        $directory = "KINTAI/{$currentMonth}";
-
-        // ディレクトリを作成
-        $disk->makeDirectory($directory);
-
-        // 所有者を設定 (Apache ユーザーと katahira グループ)
-        $owner = 'apache';
-        $group = 'katahira';
-        $path = $disk->path($directory);
-        exec("chown -R {$owner}:{$group} {$path}");
-
-        // パーミッションを設定 (775)
-        $permissions = 0775;
-        exec("chmod -R {$permissions} {$path}");
+        Storage::disk('sakura-vps')->makeDirectory("KINTAI/{$currentMonth}");
     }
 }
