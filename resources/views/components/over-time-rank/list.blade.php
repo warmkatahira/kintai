34070e9fa798a548c7c9466eb5d0ec2002f9df0e
warmkatahira <t.katahira@warm.co.jp>
2023-06-27 13:35:24 +0900

    20230627_1

--- resources/views/components/over-time-rank/list.blade.php
+++ resources/views/components/over-time-rank/list.blade.php
@@ -4,7 +4,6 @@
         <table class="text-sm block whitespace-nowrap">
             <thead>
                 <tr class="text-left text-white bg-gray-600 sticky top-0">
-                    <th class="font-thin py-3 px-2 text-center">詳細</th>
                     <th class="font-thin py-3 px-2 text-center">Rank</th>
                     <th class="font-thin py-3 px-2 text-center">年月</th>
                     <th class="font-thin py-3 px-2 text-center">拠点</th>
@@ -17,9 +16,6 @@
             <tbody class="bg-white">
                 @foreach($employees as $employee)
                     <tr class="text-left hover:bg-theme-sub cursor-default">
-                        <td class="py-1 px-2 border text-center">
-                            <a href="{{ route('employee_mgt.detail', ['employee_id' => $employee->employee_id]) }}" class="text-center border border-blue-500 bg-blue-100 text-blue-500 text-xs px-2 py-1">詳細</a>
-                        </td>
                         <td class="py-1 px-2 border text-center">{{ sprintf('%03d', $employees->firstItem() + $loop->index) }}</td>
                         <td class="py-1 px-2 border text-center">{{ \Carbon\CarbonImmutable::parse($employee->date)->isoFormat('YYYY年MM月') }}</td>
                         <td class="py-1 px-2 border text-center">{{ $employee->base->base_name }}</td>
