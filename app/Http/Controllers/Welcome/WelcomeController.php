2fce9cc7ff68b5c2f3303015118f857b67f230de
warmkatahira <t.katahira@warm.co.jp>
2023-10-03 09:10:46 +0900

    20231002_1

--- app/Http/Controllers/Welcome/WelcomeController.php
+++ app/Http/Controllers/Welcome/WelcomeController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Welcome;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        // ログインされている場合
+        if(Auth::check()){
+            return redirect()->route('top.index');
+        }
+        // ログインされていない場合
+        if(!Auth::check()){
+            return view('welcome');
+        }
     }
 }
