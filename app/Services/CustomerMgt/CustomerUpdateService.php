9b8102bdc2ee87dad19c74948227993a9ebf4ba8
warmkatahira <t.katahira@warm.co.jp>
2023-10-12 08:45:56 +0900

    20231012_customer_update_base_pulldown

--- app/Services/CustomerMgt/CustomerUpdateService.php
+++ app/Services/CustomerMgt/CustomerUpdateService.php
@@ -11,10 +11,13 @@ class CustomerUpdateService
 {
     public function updateCustomer($request)
     {
+        // 現在の内容を取得
+        $customer = Customer::getSpecify($request->customer_id)->first();
+        // 更新処理(荷主グループIDは更新前後の拠点IDが違えばnull更新する)
         Customer::getSpecify($request->customer_id)->update([
             'base_id' => $request->base_id,
             'customer_name' => $request->customer_name,
-            'customer_group_id' => $request->customer_group_id,
+            'customer_group_id' => $customer->base_id == $request->base_id ? $request->customer_group_id : null,
             'is_available' => $request->is_available,
             'customer_sort_order' => $request->customer_sort_order,
         ]);
