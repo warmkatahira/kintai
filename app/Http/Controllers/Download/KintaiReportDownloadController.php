6a849ab05b0bc720b442f8ab316489408d5e6b29
warmkatahira <t.katahira@warm.co.jp>
2024-09-02 10:24:40 +0900

    a

--- app/Http/Controllers/Download/KintaiReportDownloadController.php
+++ app/Http/Controllers/Download/KintaiReportDownloadController.php
@@ -53,6 +53,6 @@ class KintaiReportDownloadController extends Controller
         // PDF出力ビューに情報を渡す
         $pdf = $KintaiReportDownloadService->passDownloadInfo($kintais, $request->date, $base, $over40, $holidays, $taiyo_working_times);
         // ファイル名を設定してPDFをダウンロード
-        return $pdf->download($filename);
+        return $pdf->stream($filename);
     }
 }
